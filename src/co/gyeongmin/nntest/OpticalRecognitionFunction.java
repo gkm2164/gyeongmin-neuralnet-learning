adc2a4bfe605a1f7d14b7acd073e7ccf15e74837
Gyeongmin Go <gkm2164@gmail.com>
2016-05-02 14:27:03 +0900

    Optical Recognition Problem ... 필기 인식 어렵다.

--- src/co/gyeongmin/nntest/OpticalRecognitionFunction.java
+++ src/co/gyeongmin/nntest/OpticalRecognitionFunction.java
@@ -18,22 +18,18 @@ public class OpticalRecognitionFunction {
     private List<TestSet> testSetList;
     public OpticalRecognitionFunction(String filename) throws IOException {
         this.filename = filename;
-        this.neuralNetLearning = new NeuralNetLearning(0.45, 0.9, new int[]{256, 256, 256}, 256, 1);
+        this.neuralNetLearning = new NeuralNetLearning(0.45, 0.9, new int[]{128, 64, 32, 16, 8, 4, 2}, 256, 1);
         this.testSetList = new ArrayList<>();
 
         study();
     }
 
-    void study() throws IOException {
+    private void readTestSetList() throws IOException {
+        int iterate = 10000;
         BufferedReader br = new BufferedReader(new FileReader(filename));
 
-        while (true) {
+        for (String temp = br.readLine(); iterate-- > 0 && temp != null; temp = br.readLine()) {
             String[] lines = new String[32];
-            String temp;
-            temp = br.readLine();
-            if (temp == null) {
-                break;
-            }
 
             lines[0] = temp;
             for (int i = 1; i < 32; i++) {
@@ -44,20 +40,33 @@ public class OpticalRecognitionFunction {
             temp = temp.replaceAll(" ", "");
             int value = Integer.valueOf(temp);
 
-            TestSet ts = new TestSet();
-
-            ts.saveData(lines);
-            ts.answer = value;
-
+            TestSet ts = new TestSet(lines, value);
             testSetList.add(ts);
         }
+    }
 
-        for (int i = 0; i < 1000; i++) {
+    void study() throws IOException {
+        readTestSetList();
+
+        for (int i = 0; i < 100; i++) {
             testSetList.forEach(ts -> {
                 neuralNetLearning.forward(ts.data);
-                neuralNetLearning.backPropagate(new double[]{(ts.answer / 9.0)});
+                neuralNetLearning.backPropagate(new double[]{(ts.answer / 10.0)});
             });
+            System.out.println ("#" + i + " times studied.");
         }
+    }
+
+    public double areaAverage(byte[][] data, int startX, int startY, int lenX, int lenY) {
+        int size = lenX * lenY;
+        double sum = 0.0;
+        for (int i = startX; i < lenX; i++) {
+            for (int j = startY; j < lenY; j++) {
+                sum += data[i][j];
+            }
+        }
+
+        return sum / size;
     }
 
     public int number(byte[][] data) {
@@ -66,29 +75,40 @@ public class OpticalRecognitionFunction {
 
         for (int i = 0; i < data.length; i += 2) {
             for (int j = 0; j < data[i].length; j += 2) {
-                dblData[i / 2 * 16 + j / 2] = (data[i][j] + data[i][j + 1] + data[i + 1][j] + data[i + 1][j + 1] - 4 * '0') / 4.0;
+                int iIdx = i / 2;
+                int jIdx = j / 2;
+                double value = areaAverage(data, i, j, 2, 2);
+                dblData[iIdx * 16 + jIdx] = value;
+
+                System.out.println("Value: " + value);
             }
         }
 
         neuralNetLearning.forward(dblData);
-        return (int)Math.round(neuralNetLearning.getOutput()[0] * 9.0);
+        return (int)Math.round(neuralNetLearning.getOutput()[0] * 10.0);
     }
 
     class TestSet {
-        double[] data = new double[16 * 16];
-        int answer;
+        private double[] data;
+        private int answer;
 
-        void saveData(String[] lines) {
+        public TestSet(String[] lines, int value) {
+
+            data = new double[16 * 16];
+            saveData(lines);
+            this.answer = value;
+        }
+
+        private void saveData(String[] lines) {
+            byte[][] tempData = new byte[lines.length][];
+
+            for (int i = 0; i < tempData.length; i++) {
+                tempData[i] = lines[i].getBytes();
+            }
+
             for (int i = 0; i < lines.length; i += 2) {
                 for (int j = 0; j < 32; j += 2) {
-                    int d1 = lines[i].getBytes()[j] - '0',
-                        d2 = lines[i].getBytes()[j + 1] - '0',
-                        d3 = lines[i + 1].getBytes()[j] - '0',
-                        d4 = lines[i + 1].getBytes()[j + 1] - '0';
-
-                    double value = d1 + d2 + d3 + d4;
-                    value /= 4;
-                    data[i / 2 * 16 + j / 2] = value;
+                    data[i / 2 * 16 + j / 2] = areaAverage(tempData, i, j, 2, 2) - '0';
                 }
             }
         }
